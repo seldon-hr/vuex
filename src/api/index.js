ca33ac855683b2dc758f95f82242f03f27bd5f68
seldon-hr1 <daanmaro99june@gmail.com>
2024-10-25 23:42:38 -0500

    feat(api): "Add service login"

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,31 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+
+export const login = async (itemRequestLogin) => {
+    try {
+        const response = await fetch(`${BASE_URL}users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(itemRequestLogin),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to login');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+
 export const gettingUsers = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const res = await fetch(`${BASE_URL}users`);
     const data = await res.json();
     return data;
 }
+
+
