96ced63355a202446da56f34a0180bf19096a31c
seldon-hr1 <daanmaro99june@gmail.com>
2024-10-26 11:43:58 -0500

    fix(socket): "set config"

--- src/services/socket.js
+++ src/services/socket.js
@@ -6,12 +6,81 @@ class SocketService {
         this.socket = null;
     }
 
-    //Inicializar el socket
+    // Inicializar socket
     connect() {
         this.socket = io('http://localhost:3000', {
             auth: {
-                token: store.state.auth.token
+                token: localStorage.getItem('token')
             }
         });
+
+        // Eventos de conexión
+        this.socket.on('connect', () => {
+            store.commit('messages/SET_CONNECTED', true);
+        });
+
+        this.socket.on('disconnect', () => {
+            store.commit('messages/SET_CONNECTED', false);
+        });
+
+        // Eventos de mensajes
+        this.socket.on('new_message', (message) => {
+            store.commit('messages/ADD_MESSAGE', message);
+        });
+
+        // Eventos de usuarios
+        this.socket.on('user_joined', (user) => {
+            store.commit('contacts/ADD_ACTIVE_USER', user);
+        });
+
+        this.socket.on('user_left', (userId) => {
+            store.commit('contacts/REMOVE_ACTIVE_USER', userId);
+        });
+
+        // Eventos de escritura
+        this.socket.on('typing_start', ({ userId, channelId }) => {
+            store.commit('channels/SET_TYPING', { userId, channelId, isTyping: true });
+        });
+
+        this.socket.on('typing_end', ({ userId, channelId }) => {
+            store.commit('channels/SET_TYPING', { userId, channelId, isTyping: false });
+        });
+    }
+
+    // Enviar mensaje
+    sendMessage(channelId, message) {
+        if (this.socket) {
+            this.socket.emit('send_message', {
+                channelId,
+                content: message
+            });
+        }
+    }
+
+    // Unirse a un canal
+    joinChannel(channelId) {
+        if (this.socket) {
+            this.socket.emit('join_channel', channelId);
+        }
+    }
+
+    // Indicar que está escribiendo
+    sendTyping(channelId, isTyping) {
+        if (this.socket) {
+            this.socket.emit(
+                isTyping ? 'typing_start' : 'typing_end',
+                { channelId }
+            );
+        }
+    }
+
+    // Desconectar socket
+    disconnect() {
+        if (this.socket) {
+            this.socket.disconnect();
+            this.socket = null;
+        }
     }
 }
+
+export default new SocketService();
