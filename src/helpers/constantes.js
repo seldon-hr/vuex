16007b12e0f4656c2018ec6bb003dd5cbe866eb6
Seldon <daanmaro99june@gmail.com>
2025-03-25 17:13:05 -0600

    feat(constants): Add a new file for general constants and backend error codes

---
+++ src/helpers/constantes.js
@@ -0,0 +1,4 @@
+/* Dentro de este archivo vamos a almacenar para que dentro de toda la app si tenemos valores
+constantes generales estos van a provenir de este lado, así como códigos de error de Backend
+[x]: Códigos de Backend por crear.
+*/
